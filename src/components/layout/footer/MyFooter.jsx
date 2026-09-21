83904c18fa6256ddbe0ffa4708024cbd40613cfb
Islam95-ellaket <ellaket.islam95@gmail.com>
2026-09-21 19:03:37 +0200

    modifica al footer

--- src/components/layout/footer/MyFooter.jsx
+++ src/components/layout/footer/MyFooter.jsx
@@ -1,36 +1,54 @@
-import {Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 
 const MyFooter = () => {
-    return (
-        <footer className="bg-dark text-white mt-1 p-4 text-center">
-            <Container>
-                <Row>
-                    <Col md={4}>
-                        <h5>About Us</h5>
-                        <p>We provide excellent services and solutions.</p>
-                    </Col>
-                    <Col md={4}>
-                        <h5>Links</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="/" className="text-white">Home</a></li>
-                            <li><a href="/services" className="text-white">Services</a></li>
-                            <li><a href="/contact" className="text-white">Contact</a></li>
-                        </ul>
-                    </Col>
-                    <Col md={4}>
-                        <h5>Contact</h5>
-                        <p>Email: info@example.com</p>
-                        <p>Phone: +1234567890</p>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="mt-3">
-                        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer className="bg-dark text-white py-4 text-center mt-auto">
+      <Container>
+        <Row className="gy-3">
+          <Col md={4}>
+            <h5>EpiBooks</h5>
+            <p className="small mb-0">
+              La tua libreria digitale di fiducia. Esplora il nostro catalogo,
+              scopri nuovi autori e leggi le recensioni della community.
+            </p>
+          </Col>
+          <Col md={4}>
+            <h5>Navigazione</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="/" className="text-white text-decoration-none">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  Sfoglia Libri
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-white text-decoration-none">
+                  Informazioni
+                </a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4}>
+            <h5>Contatti & Supporto</h5>
+            <p className="small mb-1">Email: supporto@epibooks.com</p>
+            <p className="small mb-0">Tel: +39 06 123 4567</p>
+          </Col>
+        </Row>
+        <hr className="my-3 border-secondary" />
+        <Row>
+          <Col>
+            <p className="small mb-0">
+              &copy; {new Date().getFullYear()} EpiBooks. Tutti i diritti riservati.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
-export default MyFooter;
+export default MyFooter
